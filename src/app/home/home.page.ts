8c0dc0dd2621d5d951035fb57ddd4f32b9502bb0
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-11-20 16:53:26 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+declare var cloudinary;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  uploadPDF() {
+    console.log('HomePage::uploadPDF | method called');
+
+    cloudinary.openUploadWidget(
+      {
+        cloud_name: 'CLOUD_NAME',
+        upload_preset: 'UPLOAD_PRESET',
+        tags: ['pdf'],
+        sources: [
+          'local',
+          'url',
+          'camera'
+        ]
+      },
+      (error, result) => {
+        console.log(error, result);
+        // Listen to 'success' event.
+        if (result.event === 'success') {
+        }
+      }
+    );
+    }
 
 }
